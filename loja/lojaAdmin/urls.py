5d2c9637cc3c3e874ea82269cd4640498a59fbfe
carolcostaoliv <carolinaoliveiraacosta@gmail.com>
2026-04-13 11:54:08 -0300

    imagem

--- loja/lojaAdmin/urls.py
+++ loja/lojaAdmin/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 """
 URL configuration for lojaAdmin project.
 
@@ -19,4 +21,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
