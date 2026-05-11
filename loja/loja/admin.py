32d94a495d82ad793dfac8683e7c4124d7372da7
carolcostaoliv <carolnaoliveiraacosta@gmail.com>
2026-05-11 11:29:21 -0300

    cap5

--- loja/loja/admin.py
+++ loja/loja/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+
+class FabricanteAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao','preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao','msgPromocao', 'preco', 'categoria',)
+    search_fields = ('Produto',)
+
+admin.site.register(Fabricante,FabricanteAdmin)
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
